55b7cfa0d7747b98b87c913f62839e06695219fc
Arseniy Sazonov <rimus00@yandex.ru>
2024-05-19 19:11:01 +0300

    Add animation deleted

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ inputTask.addEventListener('keypress', (e) => {
 
 function addTask(task) {
   container = taskTemplate.querySelector('.tasks__one').cloneNode(true);
+  container.setAttribute('id', task);
   container.querySelector('.tasks__text').textContent = task;
   container.querySelector('.tasks__text').setAttribute('for', task);
   container.querySelector('.tasks__checkbox').id = task;
   container.querySelector('.tasks__category').textContent = 'All';
   taskContainer.append(container);
+
+  const buttonDeledetTask = container.querySelector('.task__delete');
+  buttonDeledetTask.addEventListener('click', () => {
+    deleteTask = document.querySelector(`#${task}`);
+    deleteTask.classList.add("tasks__one_delete");
+    setTimeout(() => deleteTask.remove(), 500)
+    // Сейчас возникает ошибка при которой если сразу все удалять, остается последний эл. 
+  })
 } 
